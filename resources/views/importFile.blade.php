c55364e76066ebfcdadde8bab1a6068bee6b6066
Dulon18 <dulon587@gmail.com>
2022-10-18 10:04:42 +0600

    Laravel Import and Export CSV or Excel

---
+++ resources/views/importFile.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Import Export Excel & CSV to Database in Laravel </title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+     integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container mt-5 text-center">
+        <h2 class="mb-4">
+            Laravel  Import and Export CSV & Excel to Database Example
+        </h2>
+        <form action="{{ route('file-import') }}" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group mb-4" style="max-width: 500px; margin: 0 auto;">
+                <div class="custom-file text-left">
+                    <input type="file" name="file" class="custom-file-input" id="customFile">
+                    <label class="custom-file-label" for="customFile">Choose file</label>
+                </div>
+            </div>
+            <button class="btn btn-primary"><i class="fas fa-arrow-up"></i> Import data</button>
+            <a class="btn btn-success" href="{{ route('file-export') }}"><i class="fas fa-download"></i> Export data</a>
+        </form>
+    </div>
+    <script>
+        document.querySelector('.custom-file-input').addEventListener('change',function(e){
+            var fileName = document.getElementById("customFile").files[0].name;
+            var nextSibling = e.target.nextElementSibling
+            nextSibling.innerText = fileName
+            })
+    </script>
+</body>
+</html>
